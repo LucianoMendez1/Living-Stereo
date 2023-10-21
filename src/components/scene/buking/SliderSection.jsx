8bf333d2d619ee2d959263469f110285c550a411
luciano mendez <mendezluciano23@gmail.com>
2023-10-21 03:34:18 -0300

    buking part3-cambios-hometambien

--- src/components/scene/buking/SliderSection.jsx
+++ src/components/scene/buking/SliderSection.jsx
@@ -3,13 +3,31 @@ import './sliderSection.css'
 const SliderSection = () => {
   return (
     <div>
-      <section>
- 
-        <img src="https://res.cloudinary.com/dvnhn35l4/image/upload/v1697588305/79536479_556673211579065_5494554745452327860_n_mjutxx.jpg" alt="" />
-        <img src="https://res.cloudinary.com/dvnhn35l4/image/upload/v1697749878/Agustin_Bragoni_-_Chacra_el_Descanso_2_wv4ttr.jpg" alt="" />
-        <img src="https://res.cloudinary.com/dvnhn35l4/image/upload/v1697862792/10-DSC05673_fdwtd1.jpg" alt="" />   
-      </section>
-    </div>
+    <div class="container2">
+              <div class="row">
+                <div class="column2">
+                  <img
+                    src="https://res.cloudinary.com/dvnhn35l4/image/upload/v1697588305/79536479_556673211579065_5494554745452327860_n_mjutxx.jpg"
+                    alt="Imagen 1"
+                  ></img>
+                </div>
+                <div class="column2">
+                  <img
+                    src="https://res.cloudinary.com/dvnhn35l4/image/upload/v1697749878/Agustin_Bragoni_-_Chacra_el_Descanso_2_wv4ttr.jpg"
+                    alt="Imagen 2"
+                  ></img>
+                </div>
+                <div class="column2">
+                  <img
+                    src=" https://res.cloudinary.com/dvnhn35l4/image/upload/v1697862792/10-DSC05673_fdwtd1.jpg"
+                    alt="Imagen 3"
+                  ></img>
+              
+           
+                </div>
+              </div>
+            </div>
+            </div>
   );
 };
 
