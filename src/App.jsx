d558af28c66e0ff145f01fc89572e7073e9d7adf
luciano mendez <mendezluciano23@gmail.com>
2023-10-23 15:22:08 -0300

    buking part3-cambios-hometambien

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/home/Home';
-import AgustinBragoni from './components/scene/buking/biografias/AgustinBragoni';
-import SimonDimarzio from './components/scene/buking/biografias/SimonDimarzio';
+
+const LazyAgustinBragoni = lazy(() => import('./components/scene/buking/biografias/AgustinBragoni'));
+const LazySimonDimarzio = lazy(() => import('./components/scene/buking/biografias/SimonDimarzio'));
+
 const App = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
-          <Route path="/biografias/AgustinBragoni" element={<AgustinBragoni />} />
-          <Route path="/biografias/SimonDimarzio" element={<SimonDimarzio />} />
-
+          <Route path="/biografias/AgustinBragoni" element={<LazyAgustinBragoni />} />
+          <Route path="/biografias/SimonDimarzio" element={<LazySimonDimarzio />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
