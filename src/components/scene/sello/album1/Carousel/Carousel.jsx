fca2b2255b768e081da678806ea4557414dfa5b9
luciano mendez <mendezluciano23@gmail.com>
2023-10-30 15:42:46 -0300

    actualizacion-album1

--- src/components/scene/sello/album1/Carousel/Carousel.jsx
+++ src/components/scene/sello/album1/Carousel/Carousel.jsx
@@ -1,65 +1,81 @@
-import React, { useState } from 'react'
-import CarouselItem from '../CarouselItem/CarouselItem'
-import * as styles from './Carousel.module.css'
+import React, { useState, useRef } from 'react';
+import CarouselItem from '../CarouselItem/CarouselItem';
+import * as styles from './Carousel.module.css';
 
 function Carousel() {
-    const [activeIndex, setActiveIndex] = useState(0)
+    const [activeIndex, setActiveIndex] = useState(0);
+    const audioRef = useRef(null);
+
     const items = [
         {
-            index: 0,
-            status: activeIndex === 0 ? 'active' : 'inactive',
             text: 'Nicolas Nieves, Agustín Bragoni, Simon Di Marzio - Other Side [South America Avenue]',
-            title: 'Living Stero',
-            srcLink:
-                'https://uploads-ssl.webflow.com/60c73b61baea9c518130ee68/62375338d69258c01fe12876_00_concepts-hero.jpg ',
+            title: 'Other Side',
+            srcLink: 'https://uploads-ssl.webflow.com/60c73b61baea9c518130ee68/62375338d69258c01fe12876_00_concepts-hero.jpg',
+            audioSrc: 'https://res.cloudinary.com/dvnhn35l4/video/upload/v1698687934/Nicolas_Nieves_Agust%C3%ADn_Bragoni_Simon_Di_Marzio_-_Other_Side_South_America_Avenue_zchegh.mp3',
         },
         {
-            index: 1,
-            status: activeIndex === 1 ? 'active' : 'inactive',
-            text: 'ANicolas Nieves, Agustín Bragoni, Simon Di Marzio  ',
-            title: 'Living Stero',
-            srcLink:
-                'https://uploads-ssl.webflow.com/60c73b61baea9c518130ee68/62375338d69258c01fe12876_00_concepts-hero.jpg',
+            text: 'Nicolas Nieves, Agustín Bragoni, Simon Di Marzio',
+            title: 'Rivera Paradise',
+            srcLink: 'https://uploads-ssl.webflow.com/60c73b61baea9c518130ee68/62375338d69258c01fe12876_00_concepts-hero.jpg',
+            audioSrc: 'https://res.cloudinary.com/dvnhn35l4/video/upload/v1698689471/Nicolas_Nieves_Agust%C3%ADn_Bragoni_Simon_Di_Marzio_-_Rivera_Paradise_South_America_Avenue_imwpnj.mp3',
         },
-    ]
+        {
+            text: 'Otro ítem de tu carrusel',
+            title: 'DirtyPool ',
+            srcLink: 'https://uploads-ssl.webflow.com/60c73b61baea9c518130ee68/62375338d69258c01fe12876_00_concepts-hero.jpg',
+            audioSrc: 'https://res.cloudinary.com/dvnhn35l4/video/upload/v1698689314/Nicolas_Nieves_Agust%C3%ADn_Bragoni_Simon_Di_Marzio_-_Dirty_Pool_South_America_Avenue_mqp7tw.mp3',
+        },
+    ];
+
     function handleNavigate(direction) {
+        if (audioRef.current) {
+            audioRef.current.pause(); // Pausar la reproducción del audio actual
+        }
+
         if (direction === 'left') {
-            setActiveIndex(
-                activeIndex === 0 ? items.length - 1 : activeIndex - 1
-            )
+            setActiveIndex(activeIndex === 0 ? items.length - 1 : activeIndex - 1);
         } else if (direction === 'right') {
-            setActiveIndex(
-                activeIndex === items.length - 1 ? 0 : activeIndex + 1
-            )
+            setActiveIndex(activeIndex === items.length - 1 ? 0 : activeIndex + 1);
+        }
+
+        // Cambiar la fuente de audio cuando cambias de índice
+        if (audioRef.current) {
+            audioRef.current.src = items[activeIndex].audioSrc;
+            audioRef.current.play(); // Reproducir el nuevo audio
         }
     }
 
     return (
-        <div className={`${styles.carousel}`}>
-            {items.map((item) => {
-                const classNames = [styles.item]
-                if (item.status === 'active') {
-                    classNames.push(styles.active)
-                } else {
-                    classNames.push(styles.inactive)
-                }
-                return (
-                  
-                    <div className={classNames.join(' ')}>
-                        <CarouselItem
-                            key={item.index}
-                            data-index={item.index}
-                            // eslint-disable-next-line react/jsx-no-bind
-                            onNavigate={handleNavigate}
-                            text={item.text}
-                            srcLink={item.srcLink}
-                            title={item.title}
-                        />
-                    </div>
-                )
-            })}
+        <div>
+            <div key={activeIndex}>
+                <audio controls ref={audioRef}>
+                    <source src={items[activeIndex].audioSrc} type="audio/mpeg" />
+                    Tu navegador no soporta la reproducción de audio.
+                </audio>
+            </div>
+            <div className={`${styles.carousel}`}>
+                {items.map((item, index) => {
+                    const classNames = [styles.item];
+                    if (index === activeIndex) {
+                        classNames.push(styles.active);
+                    } else {
+                        classNames.push(styles.inactive);
+
+                    }
+                    return (
+                        <div className={classNames.join(' ')} key={index}>
+                            <CarouselItem
+                                onNavigate={handleNavigate}
+                                text={item.text}
+                                srcLink={item.srcLink}
+                                title={item.title}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-    )
+    );
 }
 
-export default Carousel
+export default Carousel;
--- src/components/scene/sello/album1/CarouselItem/CarouselItem.jsx
+++ src/components/scene/sello/album1/CarouselItem/CarouselItem.jsx
@@ -1,67 +1,62 @@
-import React from 'react'
-// eslint-disable-next-line import/no-extraneous-dependencies
-import PropTypes from 'prop-types'
-import * as styles from './CarouselItem.module.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as styles from './CarouselItem.module.css';
 
 function CarouselItem({ onNavigate, text, title, srcLink }) {
-    const navigate = (direction) => {
-        // eslint-disable-next-line no-unused-expressions
-        onNavigate && onNavigate(direction) // Call the onNavigate prop with the direction argument
-    }
+  const navigate = (direction) => {
+    // Llamamos a la función onNavigate si está definida
+    onNavigate && onNavigate(direction);
+  };
 
-    return (
-        <main className={`${styles.carousel}`}>
-            <article className={`${styles.picture}`}>
-                <img src={srcLink} alt="" />
-            </article>
-            <article className={`${styles.description}`}>
-                <p>{text}</p>
-            </article>
-            <article className={`${styles.title} effect-title`} id="title">
-                <div className={`${styles.thing} effect-title`}>
-                    <h1 className="effect-title">
-                    <br /> 
-                        South America
-                        <br /> 
-                        <br />
-                     
-                        
-                        {title} ✦
-                    </h1>
-
-                    <h1 className="effect-title"><br /> ✦</h1>
-                </div>
-            </article>
-            <article className={`${styles.controls}`}>
-                <button type="button" onClick={() => navigate('left')}>
-                    <img
-                        src="src/assets/arrow_left_icon_133703.svg"
-                        alt="right"
-                        height="50px"
-                        width="50px"
-                        className="invert-color invert"
-                    />
-                </button>
-                <button type="button" onClick={() => navigate('right')}>
-                    <img
-                        src="src/assets/coolerarrow.svg"
-                        alt="right"
-                        height="50px"
-                        width="50px"
-                        className="invert-color "
-                    />
-                </button>
-            </article>
-        </main>
-    )
+  return (
+    <div className={`${styles.carousel}`}>
+      <div className={`${styles.picture}`}>
+        <img src={srcLink} alt={title} />
+      </div>
+      <div className={`${styles.description}`}>
+        <p>{text}</p>
+      </div>
+      <div className={`${styles.title} effect-title`} id="title">
+        <div className={`${styles.thing} effect-title`}>
+          <h1 className="effect-title">
+            <br /> 
+            South America
+            <br /> 
+            <br />
+            {title} ✦
+          </h1>
+          <h1 className="effect-title"><br /> ✦</h1>
+        </div>
+      </div>
+      <div className={`${styles.controls}`}>
+        <button type="button" onClick={() => navigate('left')}>
+          <img
+            src="assets/arrow_left_icon_133703.svg" // Asegúrate de que la ruta de la imagen sea correcta
+            alt="left"
+            height="50px"
+            width="50px"
+            className="invert-color invert"
+          />
+        </button>
+        <button type="button" onClick={() => navigate('right')}>
+          <img
+            src="assets/coolerarrow.svg" // Asegúrate de que la ruta de la imagen sea correcta
+            alt="right"
+            height="50px"
+            width="50px"
+            className="invert-color"
+          />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 CarouselItem.propTypes = {
-    // eslint-disable-next-line react/require-default-props
-    onNavigate: PropTypes.func,
-    text: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    srcLink: PropTypes.string.isRequired,
-}
+  onNavigate: PropTypes.func,
+  text: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  srcLink: PropTypes.string.isRequired,
+};
 
-export default CarouselItem
+export default CarouselItem;
