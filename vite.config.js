744ef96546d857d5dbeb8c70707837f82b1f4c2f
luciano mendez <mendezluciano23@gmail.com>
2023-11-11 03:16:15 -0300

    actualizacionmobileydemas

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['is-mobile'],
+    },
+  },
+};
