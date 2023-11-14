79ef618a55655f260b807b140489da0e76c53785
luciano mendez <mendezluciano23@gmail.com>
2023-11-14 15:45:30 -0300

    sello

--- src/components/scene/sello/album1/Navbar.jsx
+++ src/components/scene/sello/album1/Navbar.jsx
@@ -1,61 +1,32 @@
+// Importaciones
 import "./Navbar.scss";
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDumpsterFire } from "@fortawesome/free-solid-svg-icons";
-import {
-  faTwitter,
-  faYoutube,
-  faCodepen,
-  // faDumpsterFire
-} from "@fortawesome/free-brands-svg-icons";
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faDumpsterFire } from '@fortawesome/free-solid-svg-icons';
+
+// Componente Navbar
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div
-        id="navbar__logo-section"
-        className="navbar__section navbar__section-logo"
-      >
-        <a href="#">
-          <FontAwesomeIcon
-            className="navbar__section__logo"
-            icon={faDumpsterFire}
-          />
-        </a>
+      <div id="navbar-section" className="navbar__section navbar__section-logo">
+        {/* Contenido del logo */}
       </div>
-      <div
-        id="navbar__link-section"
-        className="navbar__section navbar__section-links"
-      >
-       
+
+      <div id="navbar__link-section" className="navbar__section navbar__section-links">
+        {/* Contenido de los enlaces */}
       </div>
-      <div
-        id="navbar__social-section"
-        className="navbar__section navbar__section-social"
-      >
-        <a href="#">
-          <FontAwesomeIcon
-            className="navbar__section__brands navbar__section__brands__twitter"
-            icon={faTwitter}
-          />
-        </a>
-        <a href="#">
-          <FontAwesomeIcon
-            className="navbar__section__brands navbar__section__brands__youtube"
-            icon={faYoutube}
-          />
-        </a>
-        <a href="#">
-          <FontAwesomeIcon
-            className="navbar__section__brands navbar__section__brands__codepen"
-            icon={faCodepen}
-          />
-        </a>
+
+      <div id="home" className="home">
+        {/* Utilizando el componente Link de react-router-dom */}
+        <Link to="/" className="home">
+          <FontAwesomeIcon icon={faHome} />
+        </Link>
       </div>
-      <div
-        id="navbar__contact-section"
-        className="navbar__section navbar__section-contact"
-      >
-        <a href="">Living Stereo</a>
+
+      <div id="navbar__contact-section" className="navbar__section navbar__section-contact">
+        <span>Living Stereo</span>
       </div>
     </nav>
   );
