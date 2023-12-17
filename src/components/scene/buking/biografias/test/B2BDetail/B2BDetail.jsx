e8bbabe16ee64de7e9fccb8198b1de42f9aed302
luciano mendez <mendezluciano23@gmail.com>
2023-12-17 20:45:50 -0300

    push b2b

--- src/components/scene/buking/biografias/test/B2BDetail/B2BDetail.jsx
+++ src/components/scene/buking/biografias/test/B2BDetail/B2BDetail.jsx
@@ -63,35 +63,35 @@ const B2BDetail = () => {
 
       <DescriptionDetail
         title1={
-          "Agustin Bragoni, es un musico productor, que dedicó toda su vida a la música desde los 5 años."
+          "Agustín Bragoni y Simón di Marzio, dos apasionados de la música electrónica"
         }
         text1={
-          "Actualmente se desempeña en la electrónica (Progressive House, Melodic Techno & Melodic House). Con sus dos formatos, DJ Set y Live Set, tocando instrumentos como la guitarra, sintetizadores y lap steel."
+          "Han dejado una huella indeleble en la escena musical argentina con sus contribuciones únicas y su dedicación inquebrantable. Juntos, representan la fusión de talento, versatilidad y una profunda conexión con la música desde edades tempranas."
         }
         img1={
           "https://res.cloudinary.com/dvnhn35l4/image/upload/v1699071423/b2bAgusSimon/CB6AD92B-1BBF-46DA-8D21-92F6A3807A9C_poodip.jpg"
         }
         title2={
-          "A lo largo de su carrera ha logrado tocar en los escenarios más reconocidos de Argentina."
+          "Su camino en los escenarios."
         }
         text2={
-          "Como el Lollapalooza, Luna Park, Cosquin Rock, Estadio Malvinas Argentinas, entre otros."
+          "La colaboración entre estos dos artistas va más allá de la mezcla de sonidos. Juntos, han explorado el género House, compartiendo escenario con reconocidos nombres como Kolombo y Nic Fanciulli. Su incursión en nuevas fronteras musicales evidencia no solo su versatilidad sino también su influencia en la evolución de la música electrónica en Argentina."
         }
         img2={
           "https://res.cloudinary.com/dvnhn35l4/image/upload/v1699071417/b2bAgusSimon/651c4a28-b78c-40c5-905b-a1d18220e3d4_wojmgp.jpg"
         }
-        title3={"Así mismo, además de su proyecto como DJ. "}
+        title3={"Agustín Bragoni es un Explorador Musical desde la Infancia "}
         text3={
-          "Es parte del equipo de producción y musicalización de Chano, un exponente dentro de la música."
+          "músico y productor argentino, dio sus primeros pasos en el mundo de la música a la temprana edad de 5 años. Con una destreza innata para la guitarra y una afinidad por los sintetizadores, Bragoni se ha destacado en la escena electrónica argentina. Su versatilidad en géneros como Progressive House y Melodic Techno lo ha llevado a conquistar  y colaborar en la producción musical de reconocidos artistas locales.  Ha compartido la cabina con destacados DJs de la escena argentina, como Mariano Mellino, Juan Hansen, Soundexile, Ezequiel Arias y Jonh Cosani, además de colaborar con artistas internacionales como Cid Inc y Einmusik."
         }
         img3={
           "https://res.cloudinary.com/dvnhn35l4/image/upload/v1699071423/b2bAgusSimon/CB8454FB-CB06-45B8-B2BB-DD8605EE7307_1_mri4fo.jpg"
         }
         title4={
-          "Por su parte es productor musical y DJ residente de Iconic Club."
+          "Simón di Marzio, músico y productor de La Plata, Argentina."
         }
         text4={
-          "En los últimos tiempos, ha tenido la oportunidad de incursionar dentro del género House."
+          " se adentró en el mundo de la música inspirado por grandes referentes como The Chemical Brothers y Hernan Cattaneo. Hace aproximadamente 6 años, marcó el inicio de su carrera en la escena electrónica, destacándose por transiciones armónicas y sonidos cautivadores. Como DJ residente en Iconic Club, ha dejado una huella sonora única, contribuyendo al pulso de la noche platense. Su incursión en la producción musical le valió el respaldo de Hernán Cattaneo en su podcast semanal Residente. La habilidad de Simón para crear atmósferas envolventes y su identificación con géneros como Progressive House, Deep House y Organic House lo han convertido en una figura respetada en la escena musical argentina. A lo largo de su trayectoria, Simón di Marzio ha compartido la cabina con diversos artistas destacados,Su visión única y su constante exploración de nuevos sonidos lo posicionan como un talento en ascenso en la escena electrónica argentina."
         }
         img4={
           "https://res.cloudinary.com/dvnhn35l4/image/upload/v1699071425/b2bAgusSimon/f7b52ae3-ad22-4702-999b-35610a63c349_tprk0m.jpg"
